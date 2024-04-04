667c85fd5fe565bf69d0e968804fd8f0c83f2571
przemyslvw <przemyslvw@gmail.com>
2024-04-04 18:03:57 +0200

    skanowanie podatności + komentarze

--- wlan_test.py
+++ wlan_test.py
@@ -1,5 +1,6 @@
 import subprocess
 
+# zwraca wartość interfejsu sieciowego
 def get_network_interface():
     return "wlan0"
     # try:
@@ -13,6 +14,22 @@ def get_network_interface():
     #     print("Error:")
     #     print(e.stderr)
 
+#  skanowanie na podatności
+def run_vulnerability_scan(ip):
+    print(f"Running vulnerability scan on host: {ip}")
+    command = ["nmap", "-v", "--script=vuln", ip]
+
+    try:
+        result = subprocess.run(command, capture_output=True, text=True, check=True)
+        print(f"Vulnerability scan of {ip} completed successfully.")
+        print("Output:")
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print(f"Vulnerability scan of {ip} failed.")
+        print("Error:")
+        print(e.stderr)
+
+# ta funkcja skanuje hosta na podstawie określonego adresu IP
 def run_detailed_nmap_scan(ip):
     print(f"Running detailed nmap scan on host: {ip}")
     command = ["nmap", "-v", "-A", "-T4", "-p-", ip]
@@ -27,6 +44,7 @@ def run_detailed_nmap_scan(ip):
         print("Error:")
         print(e.stderr)
 
+# wykonuje skanowanie Nmap na określonym zakresie adresów IP
 def run_nmap_scan(ip_range):
     network_interface = get_network_interface()
     print(f"Running nmap scan on network: {ip_range} using network interface: {network_interface}")
@@ -42,6 +60,7 @@ def run_nmap_scan(ip_range):
         print("Error:")
         print(e.stderr)
 
+#ta funkcjA sprawdza, czy określony ciąg znaków znajduje się w wynikach skanowania
 def check_scan_results_for_string(results, string):
     if results is None:
         print("No results to check.")
@@ -52,6 +71,7 @@ def check_scan_results_for_string(results, string):
     else:
         print(f"Did not find {string} in scan results.")
 
+#ta funkcja skanuje hosta na podstawie określonego adresu IP
 def run_nmap_scan(ip_range):
     network_interface = get_network_interface()
     print(f"Running nmap scan on network: {ip_range} using network interface: {network_interface}")
@@ -69,7 +89,8 @@ def run_nmap_scan(ip_range):
 
 # run_nmap_scan("192.168.0.0/24")
 
-run_detailed_nmap_scan("192.168.0.164")
+# run_detailed_nmap_scan("192.168.0.164")
 
 # results = run_detailed_nmap_scan("192.168.0.164")
 # check_scan_results_for_string(results, "OpenSSH")
+run_vulnerability_scan("192.168.0.164")
